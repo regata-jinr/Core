ed5f9f0b7a7e723c4b1cb1022fc389b40b543470
Boris Rumyantsev <bd.rumyantsev@gmail.com>
2021-09-22 22:31:24 +0300

    Move Process managment from detector to base. Try to move to nuget packages instead of handle script build and dependencies

--- src/hardware/Detector/src/G2KUtilities.cs
+++ src/hardware/Detector/src/G2KUtilities.cs
@@ -30,28 +30,24 @@
 
 using System;
 using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Text;
 
 namespace Regata.Core.Hardware
 {
     public partial class Detector : IDisposable
     {
-        private const int _timeOutSec = 15;
-
         public static async Task<ProcessResult> RunMvcgAsync()
         {
             return await ExecuteShellCommandAsync("putview.exe", @"/CXCY=-100,-100 /NO_DATASRC");
         }
 
-        public static async Task<ProcessResult> ShowDetectorInMvcgAsync(string det)
-        {
-            return await ExecuteShellCommandAsync("pvopen.exe", $"DET:{det} /READ_ONLY");
-        }
-
         public static async Task<ProcessResult> CloseMvcgAsync()
         {
             return await ExecuteShellCommandAsync("endview.exe");
+        }
+
+        public static async Task<ProcessResult> ShowDetectorInMvcgAsync(string det)
+        {
+            return await ExecuteShellCommandAsync("pvopen.exe", $"DET:{det} /READ_ONLY");
         }
 
         public static async Task<ProcessResult> SelectDetectorAsync(string det)
@@ -64,133 +60,9 @@ namespace Regata.Core.Hardware
             return await ExecuteShellCommandAsync("pvclose.exe", $"DET:{det}");
         }
 
-        public static async Task<ProcessResult> MinimizeMVCG()
-        {
-            return await ExecuteShellCommandAsync("Get-Process -Name mvcg | Set-WindowState -State MINIMIZE");
-        }
-
-        public static async Task<ProcessResult> MaximizeMVCG()
-        {
-            return await ExecuteShellCommandAsync("Get-Process -Name mvcg | Set-WindowState -State MAXIMIZE");
-        }
-
-
         private static async Task<ProcessResult> ExecuteShellCommandAsync(string command, string arguments = "")
         {
-            var result = new ProcessResult();
-
-            using (var process = new Process())
-            {
-
-                process.StartInfo.FileName = command;
-                process.StartInfo.Arguments = arguments;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardInput = true;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.WorkingDirectory = @"C:\GENIE2K\EXEFILES";
-                // Подписка на события записи в выходные потоки процесса
-
-                var outputBuilder = new StringBuilder();
-                var outputCloseEvent = new TaskCompletionSource<bool>();
-
-                process.OutputDataReceived += (s, e) =>
-                                                {
-                                                    // Поток output закрылся (процесс завершил работу)
-                                                    if (string.IsNullOrEmpty(e.Data))
-                                                    {
-                                                        outputCloseEvent.SetResult(true);
-                                                    }
-                                                    else
-                                                    {
-                                                        outputBuilder.AppendLine(e.Data);
-                                                    }
-                                                };
-
-                var errorBuilder = new StringBuilder();
-                var errorCloseEvent = new TaskCompletionSource<bool>();
-
-                process.ErrorDataReceived += (s, e) =>
-                                                {
-                                                    // Поток error закрылся (процесс завершил работу)
-                                                    if (string.IsNullOrEmpty(e.Data))
-                                                    {
-                                                        errorCloseEvent.SetResult(true);
-                                                    }
-                                                    else
-                                                    {
-                                                        errorBuilder.AppendLine(e.Data);
-                                                    }
-                                                };
-
-                bool isStarted;
-
-                try
-                {
-                    isStarted = process.Start();
-                }
-                catch (Exception error)
-                {
-                    // Usually it occurs when an executable file is not found or is not executable
-
-                    result.Completed = true;
-                    result.ExitCode = -1;
-                    result.Output = error.Message;
-
-                    isStarted = false;
-                }
-
-                if (isStarted)
-                {
-                    // Reads the output stream first and then waits because deadlocks are possible
-                    process.BeginOutputReadLine();
-                    process.BeginErrorReadLine();
-
-                    // Creates task to wait for process exit using timeout
-                    var waitForExit = WaitForExitAsync(process, _timeOutSec);
-
-                    // Create task to wait for process exit and closing all output streams
-                    var processTask = Task.WhenAll(waitForExit, outputCloseEvent.Task, errorCloseEvent.Task);
-
-                    // Waits process completion and then checks it was not completed by timeout
-                    if (await Task.WhenAny(Task.Delay(TimeSpan.FromSeconds(_timeOutSec)), processTask) == processTask && waitForExit.Result)
-                    {
-                        result.Completed = true;
-                        result.ExitCode = process.ExitCode;
-
-                        // Adds process output if it was completed with error
-                        if (process.ExitCode != 0)
-                        {
-                            result.Output = $"{outputBuilder}{errorBuilder}";
-                        }
-                    }
-                    else
-                    {
-                        try
-                        {
-                            // Kill hung process
-                            process.Kill();
-                        }
-                        catch
-                        {
-                        }
-                    }
-                }
-            }
-            return result;
-        }
-
-        private static Task<bool> WaitForExitAsync(Process process, int timeout)
-        {
-            return Task.Run(() => process.WaitForExit(timeout*1000));
-        }
-
-        public struct ProcessResult
-        {
-            public bool Completed;
-            public int? ExitCode;
-            public string Output;
+            return await Shell.ExecuteCommandAsync(command, arguments, @"C:\GENIE2K\EXEFILES");
         }
 
     } // public partial class Detector : IDisposable
